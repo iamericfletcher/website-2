f7e4c88530c18b4bf0fab71c6ffe689e49d74536
Eric Fletcher <64165327+iamericfletcher@users.noreply.github.com>
2022-05-01 10:09:15 -0400

    Added animation to each section in mobile view

--- js/main.js
+++ js/main.js
@@ -3,17 +3,16 @@ let aboutMeOnOff = 1;
 function showAboutMeDropdown() {
   if (aboutMeOnOff === 1) {
     document.getElementById("about-me-section").style.clipPath = "none"
-    document.getElementById("about-me-section").style.animationName = "fadeIn"
+    document.getElementById("about-me-section").style.animationName = "fadeInAboutMe"
     aboutMeOnOff = 0;
     document.getElementById("about-me-dropdown").style.display = "block"
     document.getElementById("about-me-section").style.height = "100%"
     document.getElementById("about-me-title").style.display = "none"
   } else {
     document.getElementById("about-me-section").style.clipPath = "polygon(0 0%, 100% 0, 100% 100%, 0 0%)"
-    document.getElementById("about-me-section").style.animationName = "fadeOut"
+    document.getElementById("about-me-section").style.animationName = "fadeOutAboutMe"
     aboutMeOnOff = 1;
     document.getElementById("about-me-dropdown").style.display = "none"
-    // document.getElementById("about-me-section").style.height = "10em"
     document.getElementById("about-me-title").style.display = "block"
   }
 }
@@ -23,15 +22,16 @@ let experienceOnOff = 1;
 function showExperienceDropdown() {
   if (experienceOnOff === 1) {
     document.getElementById("experience-section").style.clipPath = "none"
+    document.getElementById("experience-section").style.animationName = "fadeInExperience"
     experienceOnOff = 0;
     document.getElementById("experience-dropdown").style.display = "block"
     document.getElementById("experience-section").style.height = "100%"
     document.getElementById("experience-title").style.display = "none"
   } else {
     document.getElementById("experience-section").style.clipPath = "polygon(100% 0, 0 0, 0 100%, 100% 0)"
+    document.getElementById("experience-section").style.animationName = "fadeOutExperience"
     experienceOnOff = 1;
     document.getElementById("experience-dropdown").style.display = "none"
-    document.getElementById("experience-section").style.height = "10em"
     document.getElementById("experience-title").style.display = "block"
   }
 }
@@ -42,15 +42,16 @@ let projectOnOff = 1;
 function showProjectDropdown() {
   if (projectOnOff === 1) {
     document.getElementById("project-section").style.clipPath = "none"
+    document.getElementById("project-section").style.animationName = "fadeInProject"
     projectOnOff = 0;
     document.getElementById("project-dropdown").style.display = "block"
     document.getElementById("project-section").style.height = "100%"
     document.getElementById("project-title").style.display = "none"
   } else {
     document.getElementById("project-section").style.clipPath = "polygon(0 0%, 100% 0, 100% 100%, 0 0%)"
+    document.getElementById("project-section").style.animationName = "fadeOutProject"
     projectOnOff = 1;
     document.getElementById("project-dropdown").style.display = "none"
-    document.getElementById("project-section").style.height = "10em"
     document.getElementById("project-title").style.display = "block"
   }
 }
@@ -60,15 +61,16 @@ let socialOnOff = 1;
 function showSocialDropdown() {
   if (socialOnOff === 1) {
     document.getElementById("social-section").style.clipPath = "none"
+    document.getElementById("social-section").style.animationName = "fadeInSocial"
     socialOnOff = 0;
     document.getElementById("social-dropdown").style.display = "block"
     document.getElementById("social-section").style.height = "100%"
     document.getElementById("social-title").style.display = "none"
   } else {
     document.getElementById("social-section").style.clipPath = "polygon(100% 0, 0 0, 0 100%, 100% 0)"
+    document.getElementById("social-section").style.animationName = "fadeOutSocial"
     socialOnOff = 1;
     document.getElementById("social-dropdown").style.display = "none"
-    document.getElementById("social-section").style.height = "10em"
     document.getElementById("social-title").style.display = "block"
   }
 }
@@ -79,15 +81,16 @@ let contactOnOff = 1;
 function showContactDropdown() {
   if (contactOnOff === 1) {
     document.getElementById("contact-section").style.clipPath = "none"
+    document.getElementById("contact-section").style.animationName = "fadeInContact"
     contactOnOff = 0;
     document.getElementById("contact-dropdown").style.display = "block"
     document.getElementById("contact-section").style.height = "100%"
     document.getElementById("contact-title").style.display = "none"
   } else {
     document.getElementById("contact-section").style.clipPath = "polygon(0 0%, 100% 0, 100% 100%, 0 0%)"
+    document.getElementById("contact-section").style.animationName = "fadeOutContact"
     contactOnOff = 1;
     document.getElementById("contact-dropdown").style.display = "none"
-    document.getElementById("contact-section").style.height = "10em"
     document.getElementById("contact-title").style.display = "block"
   }
 }
